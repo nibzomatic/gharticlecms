24916b822251c75df7adf9f3c2f2a437291d1ed9
Neil Brewer <neil.brewer@gmail.com>
2024-12-07 12:30:18 -0500

    views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -129,6 +129,7 @@ def _build_msal_app(cache=None, authority=None):
         Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
         client_credential=Config.CLIENT_SECRET, token_cache=cache)
 
+
 def _build_auth_url(authority=None, scopes=None, state=None):
     return _build_msal_app(authority=authority).get_authorization_request_url(
          scopes or [],
